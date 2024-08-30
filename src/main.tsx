ee160c365a760f55feea51fdedea9cf5b7661832
halah.alshaikhly@gmail.com <halah.alshaikhly@gmail.com>
2024-08-29 18:46:08 -0700

    adds <MobileFoodFacilityPermit/> Page

--- src/main.tsx
+++ src/main.tsx
@@ -1,22 +1,22 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
-import { CssBaseline, StyledEngineProvider } from '@mui/material';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { MobileFoodFacilityPermit } from "./pages/MobileFoodFacilityPermit.tsx";
+import "./index.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { CssBaseline, StyledEngineProvider } from "@mui/material";
 
 const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <App />,
-    },
-  ]);
+  {
+    path: "/",
+    element: <MobileFoodFacilityPermit />,
+  },
+]);
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <StyledEngineProvider injectFirst>
       <CssBaseline />
       <RouterProvider router={router} />
     </StyledEngineProvider>
   </StrictMode>
-)
+);
---
+++ src/pages/MobileFoodFacilityPermit.tsx
@@ -0,0 +1,16 @@
+import { Box } from "@mui/material";
+
+export const MobileFoodFacilityPermit = () => {
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        flexFlow: 1,
+        minHeight: "100vh",
+        flexDirection: "column",
+        height: "100%",
+        width: "100vw",
+      }}
+    />
+  );
+};
