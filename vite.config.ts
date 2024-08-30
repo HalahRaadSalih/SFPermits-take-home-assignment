ecc9e51c1bb338ce73337df3af972a25334b2320
halah.alshaikhly@gmail.com <halah.alshaikhly@gmail.com>
2024-08-29 18:32:32 -0700

    configure vite to parse csv files

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import dsv from '@rollup/plugin-dsv' 👈
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), dsv()],
+  assetsInclude: ['**/*.csv']
 })
