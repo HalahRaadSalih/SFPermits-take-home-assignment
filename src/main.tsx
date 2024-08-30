3a2d199bd6c99c186b03b358a3cd4050f295f89b
halah.alshaikhly@gmail.com <halah.alshaikhly@gmail.com>
2024-08-29 18:04:01 -0700

    sets up the main route

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,21 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { CssBaseline, StyledEngineProvider } from '@mui/material';
+
+const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <App />,
+    },
+  ]);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <StyledEngineProvider injectFirst>
+      <CssBaseline />
+      <RouterProvider router={router} />
+    </StyledEngineProvider>
+  </StrictMode>
 )
