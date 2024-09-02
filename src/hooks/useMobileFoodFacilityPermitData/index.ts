fa86572dadebf1949a7308d5830bdc73b6012d01
halah.alshaikhly@gmail.com <halah.alshaikhly@gmail.com>
2024-09-02 16:04:31 -0700

    exports constants from useMobileFoodFacilityPermitData

--- src/hooks/useMobileFoodFacilityPermitData/index.ts
+++ src/hooks/useMobileFoodFacilityPermitData/index.ts
@@ -1,2 +1,6 @@
 export { useMobileFoodFacilityPermitData } from "./useMobileFoodFacilityPermitData";
 export type { MobileFoodFacilityPermit } from "./useMobileFoodFacilityPermitData.types";
+export {
+  DEFAULT_OFFSET,
+  PAGINATION_LIMIT,
+} from "./useMobileFoodFacilityPermitData.constants";
