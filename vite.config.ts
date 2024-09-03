2d8d8cf6989f038bbd63dd1e5fd6962e74396b5d
halah.alshaikhly@gmail.com <halah.alshaikhly@gmail.com>
2024-09-02 20:21:17 -0700

    uninstalls papaparse

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import dsv from '@rollup/plugin-dsv' 👈
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), dsv()],
-  assetsInclude: ['**/*.csv']
-})
+  plugins: [react()],
+});
